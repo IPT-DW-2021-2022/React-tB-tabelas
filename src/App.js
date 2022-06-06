4e3da4ccd67506adcf2ef6fb3c1703336fbbbed9
jcnpereira <casimiro@ipt.pt>
2022-06-06 16:46:11 +0100

    Tabela3 - dados em Props

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@
 import React from "react";
 import Tabela from './Tabela';
 import Tabela2 from "./Tabela2";
+import Tabela3 from "./Tabela3";
+
 
 class App extends React.Component {
-
-
   render() {
+// definição de dados em modo 'props'
+// estes dados têm uma grande particularidade: Não se podem alterar
+// https://www.json.org/json-en.html
+    const alunos = [
+      {
+        nome: "Ana",
+        apelido: "Silva",
+      },
+      {
+        nome: "Mário",
+        apelido: "Lopes",
+      },
+    ]
+
+
     return (
       <div className="container">
         <h1>Alunos</h1>
@@ -22,8 +37,12 @@ class App extends React.Component {
         <p>Uso de componentes internos</p>
         <Tabela2 />
 
+        <br />
+        <h4>Terceira tabela</h4>
+        <p>Uso de dados externos ao componente (Props)</p>
+        <Tabela3 dadosAlunosIN={alunos} />
 
-
+<br/><br/>
       </div>
     )
   }
---
+++ src/Tabela3.js
@@ -0,0 +1,53 @@
+/**
+ * Tabela3.js
+ */
+
+
+import React from "react";
+
+function Cabecalho() {
+    return (
+        <thead>
+            <tr>
+                <th>Nome</th>
+                <th>Apelido</th>
+            </tr>
+        </thead>
+    )
+}
+
+
+const Corpo = (props) => {
+    // 'map' funciona como um 'foreach()'
+    const linhas = props.dadosTabelaIN.map((aluno, index) => {
+        return (
+            <tr key={index}>
+                <td>{aluno.nome}</td>
+                <td>{aluno.apelido}</td>
+            </tr>
+        )
+    })
+
+    // esta é a 'resposta' do componente
+    return (<tbody>{linhas}</tbody>)
+}
+
+
+
+class Tabela3 extends React.Component {
+
+    render() {
+
+        const { dadosAlunosIN } = this.props;
+
+        return (
+            <table className="table table-striped">
+                <Cabecalho />
+                <Corpo dadosTabelaIN={dadosAlunosIN} />
+            </table>
+        )
+    }
+}
+
+
+export default Tabela3
